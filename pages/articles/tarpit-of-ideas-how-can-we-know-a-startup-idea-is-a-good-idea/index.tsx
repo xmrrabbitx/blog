28f99110cb61bc61d6f7d28f833b7226f89ff5ba
xmrrabbitx <hadi.mirzaie300@gmail.com>
2023-09-10 23:12:10 +0330

    tarpit of ideas

---
+++ pages/articles/tarpit-of-ideas-how-can-we-know-a-startup-idea-is-a-good-idea/index.tsx
@@ -0,0 +1,53 @@
+
+import Head from "next/head"
+import Link from "next/link"
+import CodeBlock from "@/pages/components/CodeBlock"
+import Paragraph from "@/pages/components/Paragraph"
+import Image from "next/image"
+import HeaderPic from "@/public/pics/tarpit-of-ideas-how-can-we-know-a-startup-idea-is-a-good-idea/HeaderPic.jpg"
+
+
+import iconIco from "@/public/logos/logo.ico"
+// eslint-disable-next-line react/no-unescaped-entities
+
+export default function index(){
+
+    return(
+
+        <div>
+            
+            <Head>
+                <title>Tarpit of Ideas: How can we know an idea is a good idea?</title>
+                <meta name="description" content="we can now retrieve as many top-selling NFT tokens from a specific contract address as we want in just one line of code." />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/logos/logo.ico" />
+            </Head>
+            <div className="container max-w-full">
+                <div className="lg:text-center md:text-center sm:text-center text-left pl-5 pr-5 mt-16 lg:text-3xl md:text-3xl sm:text-2xl text-xl font-bold">Tarpit of Ideas: How can we know an idea is a good idea?</div>
+                <div className="w-12/12 grid grid-cols-6 mt-10"> 
+                    <div className="lg:col-span-1 col-span-0"></div>
+                    < div className="lg:col-span-4 col-span-6 lg:p-5 md:p-5 sm:p-5 p-2 lg:m-0 md:m-0 sm:m-0 m-2 text-justify border-2 border-dashed border-amber-500 leading-8">
+                        
+
+                    <Image className="rounded max-w-full w-full" src={HeaderPic} alt={"no picture"} />
+
+                    
+                    <Paragraph text={<>A tarpit of ideas is a word used by Michael Seibel at YC Combinator Co. I think this word is a very interesting concept that we could see in every corner of our lives. So what does tarpit mean?</>} />
+                    <Paragraph text={<>A tarpit is a place with water and mud where animals become entangled and stuck. That is it; the bodies collapse, and more animals are attracted to them.</>} />
+                    <Paragraph text={<>Now that real life is like this, there are many ideas that attract you, but when you try them out, they are bad ideas that look good.</>} />
+                    <Paragraph text={<>You must recognize and avoid them. Get a better idea that do not have these kinds of problems.</>} />
+                    <Paragraph text={<>How can we find out which ideas are tarpits? There is a concept called consumer idea. The definition of consumer is obvious: it's a customer, user, or anyone who can use a product or an idea. Also, we have a concept, which you might have heard about, called the B2C business model, which means your product or idea will be consumed by people out there, ordinary people like you and me and all the world. If your idea is directly addressed to consumers, it's an alert—a red alert—and you could fail easily.</>} />
+                    <Paragraph text={<>Imagine creating a new social app. When was the last time you used a social network app other than brands like Facebook or TikTok? Yes, that's it. The problem is here: there is much supply (many social new apps) and very little demand (people are very busy and not interested in new apps like those ones).</>} />
+                    <Paragraph text={<>So don't fall into consumer ideas; the bar for these ideas is very high. Nothing is impossible; you might say, I know many businesses in this area, and Nothing is impossible; Yes, but competition in this area is very high, as is the bar, and you must decide with open eyes.</>} />
+                    <Paragraph text={<>The best ideas are those in which you provide services or products to other businesses, like a tool to increase performance and make businesses more efficient.</>} />
+                    <Paragraph text={<>Be aware of the bar, market, and idea, the supply and demand mechanism, and other things that swallow you into a tarpit, and remember, nothing is impossible.</>} />
+                    
+
+                    
+                    </div>
+                    <div className="lg:col-span-1 col-span-0"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
